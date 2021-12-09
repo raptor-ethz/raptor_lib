8f7811ae25b5354c80ed4002407662b40c791183
Pascal Strauch <pstrauch@ethz.ch>
2021-12-09 20:35:34 +0100

    Changed an argument name of the constructor

--- include/Quad.h
+++ include/Quad.h
@@ -8,7 +8,7 @@ class Quad : public raptor::Participant{
     // Constructor
     Quad(   std::unique_ptr<DefaultParticipant> dp, 
             std::string sub_topic_name,
-            std::string pos_pub_topic_name);
+            std::string pub_topic_name);
 
     DDSPublisher *position_pub;
 
