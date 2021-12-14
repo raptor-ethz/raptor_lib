e594d79facf25aefe43550d5cbc9da41a8720335
Pascal Strauch <pstrauch@ethz.ch>
2021-12-14 15:55:49 +0100

    Add test executable

---
+++ test/main.cpp
@@ -0,0 +1,34 @@
+#include "Quad.h"
+
+
+
+int main () {
+    // FastDDS default participant
+    std::unique_ptr<DefaultParticipant> dp = 
+        std::make_unique<DefaultParticipant>(0, "raptor");
+
+    // Maybe this needs to be changed...
+    std::string quad_id = "Quad01";
+    std::string quad_sub_topic_name = "mocap_srl_quad";
+    std::string quad_pub_topic_name = "pos_cmd";
+
+    // create instance of quad
+    Quad quad(quad_id, dp, quad_sub_topic_name, quad_pub_topic_name);
+
+
+    // These parameters will be moved to the Quad class
+    // (and maybe also to a yaml)
+    // We'll then have getters and setters to change these variables.
+    // Also, different function overloads of go_to_pos will be implemented so
+    // those values do not have to be passed and the function call is more 
+    // compact
+    float x_thresh, y_thresh, z_thresh = 0.2;
+    int delay_time = 20;
+
+    // For now, we send the drone the position cmds as follows:
+    quad.go_to_pos(-1.0, 1.0, 2.0, 
+                    x_thresh, y_thresh, z_thresh,
+                    delay_time, 2000, false);
+
+    return 0;
+}
