ed805fc07dee89aca55237c5d9c8808ff244d031
Pascal Strauch <pstrauch@ethz.ch>
2021-12-14 13:50:22 +0100

    Add file structure

--- Participants/Quad/Quad.h
+++ Participants/Quad/Quad.h
@@ -15,21 +15,25 @@ class Quad : public raptor::Participant{
     /** 
      * Send a position command to this drone with a reference position.
      * Will return once the time is reached, or, if the reached_pos_flag is
-     * set to true, the position has been reached.
+     * set to true, if the position has been reached within the threshold.
      * @param [in] x_ref : Reference position - x coordinate
      * @param [in] y_ref : Reference position - y coordinate
      * @param [in] z_ref : Reference position - z coordinate
+     * @param [in] x_thresh : maximal accepted deviation from x_ref
+     * @param [in] y_thresh : maximal accepted deviation from y_ref
+     * @param [in] z_thresh : maximal accepted deviation from z_ref
      * @param [in] max_time : The time after which the function latest ends, 
      * even if the position wasn't reached
      * @param [in] reached_pos_flag : Flag if the function should end once
      * the position was reached. If false, the function always waits for 
      * max_time before ending
-     * 
      * @returns If the position has been reached when the function ends
      * 
     **/
-    bool go_to_pos(const float x_ref, const float y_ref, const float z_ref, 
-                    const float max_time, const bool reached_pos_flag);
+    bool go_to_pos( const float x_ref, const float y_ref, const float z_ref, 
+                    const float x_thresh, const float y_thresh, 
+                    const float z_thresh, const float max_time, 
+                    const bool reached_pos_flag);
 
     private:
     cpp_msg::QuadPositionCmd pos_cmd{};
