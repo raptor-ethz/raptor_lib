efc6deec0992e69e5962622b00805b7718373f28
Pascal Strauch <pstrauch@ethz.ch>
2021-12-14 14:55:44 +0100

    Edit check_for_data function

--- Participants/Participant/Participant.cpp
+++ Participants/Participant/Participant.cpp
@@ -4,9 +4,12 @@ bool raptor::Participant::check_for_data() {
     float x;
 
     for (int i = 0; i < 30; ++i) {
-        x = this->pose_.pose.position.x;
+        x = pose_.pose.position.x;
     }
     if (x == 0.0) {
+        std::cout   << "WARNING [Participant: " << id << "]: "
+                    << "Bad data after initializiation of mocap_subscriber." 
+                    << std::endl;
         return false;
     }
     return true;
