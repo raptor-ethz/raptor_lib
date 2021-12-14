0210453feb2ed6d09d3fb547675919bc32b47a69
Pascal Strauch <pstrauch@ethz.ch>
2021-12-14 21:53:18 +0100

    Edit Gripper constructor to add const string

--- Participants/Gripper/Gripper.cpp
+++ Participants/Gripper/Gripper.cpp
@@ -1,13 +1,17 @@
 #include "Gripper.h"
 
-Gripper::Gripper(std::string &raptor_participant_id,
+Gripper::Gripper(const std::string &raptor_participant_id,
                  std::unique_ptr<DefaultParticipant> &dp,
-                 std::string &pub_topic_name) {
+                 const std::string &pub_topic_name) {
+
   id = raptor_participant_id;
+
   grip_pub = new DDSPublisher(idl_msg::QuadPositionCmdPubSubType(),
                               pub_topic_name, dp->participant());
+
   grip_pub->init();
 };
+
 Gripper::~Gripper() { 
   delete grip_pub;
   delete mocap_sub;
--- Participants/Gripper/Gripper.h
+++ Participants/Gripper/Gripper.h
@@ -3,15 +3,18 @@
 #include "Participant.h"
 
 class Gripper : public raptor::Participant {
- public:
-  Gripper(std::string &raptor_participant_id,
-          std::unique_ptr<DefaultParticipant> &dp, std::string &pub_topic_name);
-  ~Gripper();
+        public:
+                Gripper(const std::string &raptor_participant_id,
+                        std::unique_ptr<DefaultParticipant> &dp, 
+                        const std::string &pub_topic_name);
+                        
+                ~Gripper();
 
-  DDSPublisher *grip_pub;
-  void close();
-  void open();
 
- private:
-  cpp_msg::QuadPositionCmd grip_cmd{};
+                DDSPublisher *grip_pub;
+                void close();
+                void open();
+
+        private:
+                cpp_msg::QuadPositionCmd grip_cmd{};
 };
