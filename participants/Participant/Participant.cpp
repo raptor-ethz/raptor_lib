9add0b6076e7901990a0ffbc03de1073b4679113
Pascal Strauch <pstrauch@ethz.ch>
2021-12-15 15:56:56 +0100

    Add wait_for_data in check_for_data

--- participants/Participant/Participant.cpp
+++ participants/Participant/Participant.cpp
@@ -3,7 +3,8 @@
 bool raptor::Participant::check_for_data() {
     float x;
 
-    for (int i = 0; i < 30; ++i) {
+    for (int i = 0; i < 10; ++i) {
+        mocap_sub->listener->wait_for_data();
         x = pose_.pose.position.x;
     }
     if (x == 0.0) {
@@ -12,9 +13,11 @@ bool raptor::Participant::check_for_data() {
                     << std::endl;
         return false;
     } else{
-        std::cout << "check for data ok" << std::endl;
+        std::cout   << "INFO [Participant: " << id << "]: "
+                    << "Good data after initializiation of mocap_subscriber." 
+                    << std::endl;
     }
-    
+
     return true;
 }
 
