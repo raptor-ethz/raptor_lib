a260ecf6e3199d651832b5a75adb51affddf1186
Pascal Strauch <pstrauch@ethz.ch>
2022-03-06 17:16:19 +0100

    Formatting

--- participants/Quad/Quad.cpp
+++ participants/Quad/Quad.cpp
@@ -2,24 +2,24 @@
 
 Quad::Quad(const std::string &raptor_participant_id,
            std::unique_ptr<DefaultParticipant> &dp,
-           const std::string &sub_topic_name,
-           const std::string &pub_topic_name) {
+           const std::string &sub_topic_name, const std::string &pub_topic_name)
+{
   id = raptor_participant_id;
 
   /* Initialize subscribers */
   mocap_sub_ = new DDSSubscriber(idl_msg::MocapPubSubType(), &pose_,
-                                sub_topic_name, dp->participant());
-  
+                                 sub_topic_name, dp->participant());
+
   /* Initialize publishers */
   position_pub_ = new DDSPublisher(idl_msg::QuadPositionCmdPubSubType(),
-                                  pub_topic_name, dp->participant());
+                                   pub_topic_name, dp->participant());
 
   px4_action_pub_ = new DDSPublisher(idl_msg::HeaderPubSubType(),
-                                  "px4_commands", dp->participant());
-
+                                     "px4_commands", dp->participant());
 };
 
-Quad::~Quad() {
+Quad::~Quad()
+{
   delete mocap_sub_;
   delete position_pub_;
   delete px4_action_pub_;
