5705471a9d9c14b8549b3f8ca35f9d46391b4d0e
Pascal Strauch <pstrauch@ethz.ch>
2022-03-09 20:08:22 +0100

    Reformat participant info messages

--- participants/Participant/Participant.cpp
+++ participants/Participant/Participant.cpp
@@ -1,32 +1,27 @@
 #include "Participant.h"
 
-bool raptor::Participant::checkForData() {
-    float x;
-
-    for (int i = 0; i < 10; ++i) {
-        mocap_sub_->listener->wait_for_data();
-        x = pose_.pose.position.x;
-    }
-    if (x == 0.0) {
-        std::cout   << "WARNING [Participant: " << id << "]: "
-                    << "Bad data after initializiation of mocap_subscriber." 
-                    << std::endl;
-        return false;
-    } else{
-        std::cout   << "INFO [Participant: " << id << "]: "
-                    << "Good data after initializiation of mocap_subscriber." 
-                    << std::endl;
-    }
-
-    return true;
-}
-
-const cpp_msg::Mocap& raptor::Participant::getPose()
+bool raptor::Participant::checkForData()
 {
-    return pose_;
+  float x;
+
+  for (int i = 0; i < 10; ++i) {
+    mocap_sub_->listener->wait_for_data();
+    x = pose_.pose.position.x;
+  }
+  if (x == 0.0) {
+    std::cout << "[ERROR][Participant: " << id << "]: "
+              << "Bad data after initializiation of mocap_subscriber."
+              << std::endl;
+    return false;
+  } else {
+    std::cout << "[INFO][Participant: " << id << "]: "
+              << "Good data after initializiation of mocap_subscriber."
+              << std::endl;
+  }
+
+  return true;
 }
 
-const std::string& raptor::Participant::getId()
-{
-    return id;
-}
+const cpp_msg::Mocap &raptor::Participant::getPose() { return pose_; }
+
+const std::string &raptor::Participant::getId() { return id; }
