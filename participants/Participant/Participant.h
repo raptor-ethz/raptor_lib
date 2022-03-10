8d8314b0c0b9b3f99fd38a00c0497c4c53075e7a
Pascal Strauch <pstrauch@ethz.ch>
2022-03-10 17:02:30 +0100

    Add log flag

--- participants/Participant/Participant.h
+++ participants/Participant/Participant.h
@@ -3,34 +3,40 @@
 #include "MocapPubSubTypes.h"
 #include "QuadPositionCmdPubSubTypes.h"
 #include "domain_participant.h"
-#include "publisher.h"
-#include "subscriber.h"
 #include "geometry_msgs/msgs/Position.h"
+#include "publisher.h"
 #include "quadcopter_msgs/msgs/QuadPositionCmd.h"
 #include "sensor_msgs/msgs/Mocap.h"
 #include "std_msgs/msgs/Header.h"
+#include "subscriber.h"
 
-namespace raptor{
+namespace raptor
+{
 
-    class Participant {
-        public:
-            DDSSubscriber<idl_msg::MocapPubSubType, cpp_msg::Mocap> *mocap_sub_;
+enum LogFlag { run, stop, bookmark };
 
-            /**
-             * Reads 30 datapoints from the subscriber and 
-             * checks for a non-zero last datapoint.
-             * 
-             * @returns if (position.x != 0.0)
-            **/
-            virtual bool checkForData();
+class Participant
+{
+public:
+  DDSSubscriber<idl_msg::MocapPubSubType, cpp_msg::Mocap> *mocap_sub_;
 
-            virtual const cpp_msg::Mocap& getPose();
+  /**
+   * Reads 30 datapoints from the subscriber and
+   * checks for a non-zero last datapoint.
+   *
+   * @returns if (position.x != 0.0)
+   **/
+  virtual bool checkForData();
 
-            virtual const std::string& getId();
+  virtual const cpp_msg::Mocap &getPose();
 
-        protected:
-            std::string id = "N/A";
-            cpp_msg::Mocap pose_{};
-    };
+  virtual const std::string &getId();
 
-}
+  LogFlag log_flag_ = run;
+
+protected:
+  std::string id = "N/A";
+  cpp_msg::Mocap pose_{};
+};
+
+} // namespace raptor
