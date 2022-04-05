e1095b33f958705bc6e8b463679f139ae152fcd8
aurel <appiusa@student.ethz.ch>
2022-04-05 11:11:40 +0200

    added gripper sensor feedback [needs to be tested]

--- participants/Gripper/Gripper.cpp
+++ participants/Gripper/Gripper.cpp
@@ -2,15 +2,19 @@
 
 Gripper::Gripper(const std::string &raptor_participant_id,
                  std::unique_ptr<DefaultParticipant> &dp,
-                 const std::string &pub_topic_name) {
+                 const std::string &pub_topic_name)
+{
   id_ = raptor_participant_id;
   grip_action_pub_ = new DDSPublisher(idl_msg::RotGripCmd_msgPubSubType(),
                                       pub_topic_name, dp->participant());
+  grip_sensor_sub_ = new DDSSubscriber(idl_msg::GripperSensor_msgPubSubType(), &grip_sensor_msg_, SENSOR_TOPIC_,
+                                       dp->participant());
 };
 
 Gripper::~Gripper() { delete grip_action_pub_; }
 
-void Gripper::setAngleSym(int angle) {
+void Gripper::setAngleSym(int angle)
+{
   // check argument feasability
   // TODO change to feedback
   assert(angle >= 0 && angle <= MAX_ANGLE);
@@ -20,14 +24,16 @@ void Gripper::setAngleSym(int angle) {
   // grip_action_cmd_.trigger_gripper = false;
   grip_action_pub_->publish(grip_action_cmd_);
 
-  if (debug) {
+  if (debug)
+  {
     std::cout << "[DEBUG][Participant: " << id_
               << "] Set angle: [ Front: " << angle << "; Back: " << angle
               << "]." << std::endl;
   }
 }
 
-void Gripper::setAngleAsym(int front_angle, int back_angle) {
+void Gripper::setAngleAsym(int front_angle, int back_angle)
+{
   // check argument feasability
   // TODO change to feedback
   assert(front_angle >= 0 && front_angle <= MAX_ANGLE);
@@ -38,9 +44,29 @@ void Gripper::setAngleAsym(int front_angle, int back_angle) {
   // grip_action_cmd_.trigger_gripper = false;
   grip_action_pub_->publish(grip_action_cmd_);
 
-  if (debug) {
+  if (debug)
+  {
     std::cout << "[DEBUG][Participant: " << id_
               << "] Set angle: [ Front: " << front_angle
               << "; Back: " << back_angle << "]." << std::endl;
   }
+}
+
+void Gripper::triggerGripper()
+{
+  grip_action_cmd_.trigger_gripper = true;
+  grip_action_pub_->publish(grip_action_cmd_);
+  grip_action_cmd_.trigger_gripper = false; // only publish once
+}
+
+void Gripper::updateSensor()
+{
+  grip_action_cmd_.get_sensor_val = true;
+  grip_action_pub_->publish(grip_action_cmd_);
+  grip_action_cmd_.get_sensor_val = false; // only publish once
+}
+
+std::vector<int> Gripper::getSensorVal()
+{
+  return std::vector<int>{grip_sensor_msg_.force_back_left, grip_sensor_msg_.force_back_right, grip_sensor_msg_.force_front_left, grip_sensor_msg_.force_front_right};
 }
