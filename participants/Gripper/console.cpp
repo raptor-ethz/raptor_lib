b946d95208917e2183f4d2b35ce871d33ab13837
Pascal Strauch <pstrauch@ethz.ch>
2022-05-10 13:42:43 +0200

    added guard clause to log pointer

--- participants/Gripper/console.cpp
+++ participants/Gripper/console.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 
 void Gripper::consoleDebug(const std::string &message) {
-  *log_ += "[" + getTime() + "|DEBUG][" + id_ + "] " + message + '\n';
+  if (log != nullptr) {
+    *log_ += "[" + getTime() + "|DEBUG][" + id_ + "] " + message + '\n';
+  }
 
   if (console_state_ == 0) {
     std::cout << "\033[1m[" << getTime() << "|DEBUG]\033[1;36m[" << id_
@@ -12,8 +14,10 @@ void Gripper::consoleDebug(const std::string &message) {
 }
 
 void Gripper::consoleInformation(const std::string &message) {
-  *log_ += "[" + getTime() + "|INFO][" + id_ + "] " + message + '\n';
-
+  if (log != nullptr) {
+    *log_ += "[" + getTime() + "|INFO][" + id_ + "] " + message + '\n';
+  }
+  
   if (console_state_ <= 1) {
     std::cout << "\033[1;32m[" << getTime() << "|INFO]\033[1;36m[" << id_
               << "] \033[1;32m" << message << "\033[0m" << std::endl;
@@ -21,7 +25,9 @@ void Gripper::consoleInformation(const std::string &message) {
 }
 
 void Gripper::consoleWarning(const std::string &message) {
-  *log_ += "[" + getTime() + "|WARNING][" + id_ + "] " + message + '\n';
+  if (log != nullptr) {
+    *log_ += "[" + getTime() + "|WARNING][" + id_ + "] " + message + '\n';
+  }
 
   if (console_state_ <= 2) {
     std::cout << "\033[1;33m[" << getTime() << "|WARNING]\033[1;36m[" << id_
@@ -30,7 +36,9 @@ void Gripper::consoleWarning(const std::string &message) {
 }
 
 void Gripper::consoleError(const std::string &message) {
-  *log_ += "[" + getTime() + "|ERROR][" + id_ + "] " + message + '\n';
+  if (log != nullptr) {
+    *log_ += "[" + getTime() + "|ERROR][" + id_ + "] " + message + '\n';
+  }
 
   if (console_state_ <= 3) {
     std::cout << "\033[1;31m[" << getTime() << "|ERROR]\033[1;36m[" << id_
